ecbec554f34761648f491d7eeb746268cd21dbca
Cesar Cobo <cesar.cobop@gmail.com>
2021-09-19 10:47:27 -0500

    -adds TypeScript configuration -adds Eslint configuration -adds Prettier configuration -adds Husky hooks for pre-commit checks

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Head from "next/head";
+
+/**
+ * Home: The Landing page of the web app
+ * @return {JSX.Element} The JSX Code for the Home Page
+ */
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Mobile Map</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className="title">Welcome to Mobile Map</h1>
+
+        <p className="description">Under Construction</p>
+      </main>
+    </div>
+  );
+}
